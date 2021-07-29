f0491f9eb99ee54839e33a64cb2c9a90d9526d3c
Vitor <vi.souza.almeida@protonmail.com>
2021-07-29 01:43:10 -0300

    feat: parte de serviços

--- components/Main.js
+++ components/Main.js
@@ -7,12 +7,12 @@ export default function Main() {
       <section className="text-gray-600 body-font">
         <div className="container px-5 mx-auto">
           <div className="text-center mb-20">
-            <h2 className="text-5xl font-medium title-font text-white mb-4">
+            {/*<h2 className="text-5xl font-medium title-font text-white mb-4">
               Sobre Mim
-            </h2>
+            </h2>*/}
             <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s text-white">
-              Oi :) <br /> <br />
-              Programo algo mais ou menos desde os meus 11 anos, mas só de uns 3 anos para cá que me dediquei com seriedade em desenvolvimento e curti o Frontend Web. Em 2020 participei do meu primeiro evento de programação Hacktoberfest e consegui concluir com os desafios e ganhar brindes do evento.
+              <h2 className = "font-medium title-font text-2xl">Oi :)</h2> <br /> <br />
+              <span className = "text-lg font-medium">Programo algo mais ou menos desde os meus 11 anos, mas só de uns 3 anos para cá que me dediquei com seriedade em desenvolvimento e curti o Frontend Web. Em 2020 participei do meu primeiro evento de programação Hacktoberfest e consegui concluir com os desafios e ganhar brindes do evento.</span>
             </p>
             <h2 className="text-2xl text-center text-white font-medium title-font pt-5">
               Tecnologias: <br />
@@ -184,7 +184,6 @@ export default function Main() {
                 </div>
                 <div className="p-4 lg:w-1/4 md:w-1/2">
                   <div className="h-full flex flex-col items-center text-center">
-
                     <div className="w-full">
                       <h2 className="title-font font-medium text-lg text-white">
                         <a className='text-purple-500 hover:text-purple-700' href="https://github.com/vit0rr/blog">Blog</a>
@@ -233,6 +232,172 @@ export default function Main() {
                     </div>
                   </div>
                 </div>
+              </div>
+            </div>
+
+            <h2 className="text-5xl font-medium title-font text-white mb-4 pt-20">
+              Serviços
+            </h2>
+            <div className="container px-5 py-5 mx-auto">
+              <div className="flex flex-wrap -m-4">
+                <div className="p-4 lg:w-1/4 md:w-1/2">
+                  <div className="h-full flex flex-col items-center text-center">
+                  </div>
+                </div>
+                <div className="p-4 lg:w-1/4 md:w-1/2">
+                  <div className="h-full flex flex-col items-center text-center">
+
+                    <div className="w-full">
+                      <h2 className="title-font font-medium text-lg text-white">
+                        <a className='text-purple-500 hover:text-purple-700' href="https://github.com/vit0rr/phogtech">Brilho da Infância</a>
+                      </h2>
+
+                      <p className="mb-4">
+                        Ecommerce desenvolvido pela Phog Tech onde configuramos desde o DNS e SEO até a loja propriamente dita.
+                      </p>
+                      <span className="inline-flex">
+                        <a className="text-gray-500">
+                          <svg
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            className="w-5 h-5"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                          </svg>
+                        </a>
+                        <a className="ml-2 text-gray-500">
+                          <svg
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            className="w-5 h-5"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                          </svg>
+                        </a>
+                        <a className="ml-2 text-gray-500">
+                          <svg
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            className="w-5 h-5"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                          </svg>
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="p-4 lg:w-1/4 md:w-1/2">
+                  <div className="h-full flex flex-col items-center text-center">
+
+                  <div className="w-full">
+                      <h2 className="title-font font-medium text-lg text-white">
+                        <a className='text-purple-500 hover:text-purple-700' href="https://github.com/vit0rr/DiscordDMNuke">Koisinhas da Kisy</a>
+                      </h2>
+
+                      <p className="mb-4">
+                      Ecommerce desenvolvido pela Phog Tech onde configuramos a loja e resolução de bugs, além de SEO.
+                      </p>
+                      <span className="inline-flex">
+                        <a className="text-gray-500">
+                          <svg
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            className="w-5 h-5"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                          </svg>
+                        </a>
+                        <a className="ml-2 text-white">
+                          <svg
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            className="w-5 h-5"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                          </svg>
+                        </a>
+                        <a className="ml-2 text-gray-500">
+                          <svg
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            className="w-5 h-5"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                          </svg>
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+{/*                <div className="p-4 lg:w-1/4 md:w-1/2">
+                  <div className="h-full flex flex-col items-center text-center">
+                    <div className="w-full">
+                      <h2 className="title-font font-medium text-lg text-white">
+                        <a className='text-purple-500 hover:text-purple-700' href="https://github.com/vit0rr/blog">Blog</a>
+                      </h2>
+                      <p className="mb-4">
+                        Meu blog onde posto textos sobre programação envolvendo algo que aprendi e gosto.
+                      </p>
+                      <span className="inline-flex">
+                        <a className="text-gray-500">
+                          <svg
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            className="w-5 h-5"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                          </svg>
+                        </a>
+                        <a className="ml-2 text-gray-500">
+                          <svg
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            className="w-5 h-5"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                          </svg>
+                        </a>
+                        <a className="ml-2 text-gray-500">
+                          <svg
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            className="w-5 h-5"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                          </svg>
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </div>*/}
               </div>
             </div>
           </div>
