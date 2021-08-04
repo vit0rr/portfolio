ef106a4418a3b6616b4f229ccb514f602976082f
Vitor <vi.souza.almeida@protonmail.com>
2021-08-03 23:04:29 -0300

    refactor: removendo SVG como componente

--- components/Projects.js
+++ components/Projects.js
@@ -1,13 +1,15 @@
-import ClickIcon from './ClickIcon'
 
-export default function Projects () {
+
+export default function Projects() {
     return (
         <section className="container mx-auto py-10 md:py-20 antialiased ">
             <section className="grid lg:grid-cols-2 2xl:grid-cols-4 grid-cols-1 gap-8">
                 <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
 
                     <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
-                        <a href="https://github.com/vit0rr/DiscordDMNuke" target="_blank"><ClickIcon></ClickIcon> DiscordDMNuke</a>
+                        <a href="https://github.com/vit0rr/DiscordDMNuke" target="_blank"><div className="inline"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                        </svg></div> DiscordDMNuke</a>
                     </h2>
                     <p className="m-4 text-lg p-4 leading-relaxed text-center ">
                         Script desenvolvido em JavaScript que remove mensagens enviadas via DM no Discord automaticamente.
@@ -16,7 +18,9 @@ export default function Projects () {
 
                 <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
                     <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
-                        <a href="https://github.com/vit0rr/phogtech" target="_blank"><ClickIcon></ClickIcon> Phog Tech</a>
+                        <a href="https://github.com/vit0rr/phogtech" target="_blank"><div className="inline"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                        </svg></div> Phog Tech</a>
                     </h2>
                     <p className="m-4 text-lg p-4 leading-relaxed text-center">
                         Projeto com mais dois amigos onde realizamos serviços de construção/manutenção de websites.
@@ -25,7 +29,9 @@ export default function Projects () {
 
                 <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
                     <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
-                        <a href="https://github.com/vit0rr/privacyforall" target="_blank"><ClickIcon></ClickIcon> Privacy For All</a>
+                        <a href="https://github.com/vit0rr/privacyforall" target="_blank"><div className="inline"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                        </svg></div> Privacy For All</a>
                     </h2>
                     <p className="m-4 text-lg p-4 leading-relaxed text-center ">
                         Projeto desenvolvido com foco em oferecer alternativas privadas para serviços comuns.
@@ -35,7 +41,9 @@ export default function Projects () {
                 <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
 
                     <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
-                        <a href="https://github.com/vit0rr/blog" target="_blank"><ClickIcon></ClickIcon> Blog</a>
+                        <a href="https://github.com/vit0rr/blog" target="_blank"><div className="inline"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                        </svg></div> Blog</a>
                     </h2>
                     <p className="m-4 text-lg p-4 leading-relaxed text-center ">
                         Meu blog onde posto alguns textos técnicos sobre programação.
@@ -46,7 +54,9 @@ export default function Projects () {
             <section className="grid lg:grid-cols-2 2xl:grid-cols-2 grid-cols-1 gap-8 pt-4">
                 <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
                     <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
-                        <a href="https://github.com/vit0rr/FlappyJS" target="_blank"><ClickIcon></ClickIcon> FlappyJS</a>
+                        <a href="https://github.com/vit0rr/FlappyJS" target="_blank"><div className="inline"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                        </svg></div> FlappyJS</a>
                     </h2>
                     <p className="m-4 text-lg p-4 leading-relaxed text-center ">
                         Jogo do Flappy Bird utilizando Canvas.
@@ -56,7 +66,9 @@ export default function Projects () {
                 <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
 
                     <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
-                        <a href="https://github.com/vit0rr/SnakeJS" target="_blank"><ClickIcon></ClickIcon> SnakeJS</a>
+                        <a href="https://github.com/vit0rr/SnakeJS" target="_blank"><div className="inline"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                        </svg></div> SnakeJS</a>
                     </h2>
                     <p className="m-4 text-lg p-4 leading-relaxed text-center ">
                         Snake Game desenvolvido em JavaScript.
--- components/Services.js
+++ components/Services.js
@@ -1,4 +1,3 @@
-import ClickIcon from "./ClickIcon"
 
 export default function Services() {
     return (
@@ -11,7 +10,9 @@ export default function Services() {
 
                     <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
                         <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
-                            <a href="https://www.brilhodainfancia.com.br/" target="_blank"><ClickIcon></ClickIcon> Brilho da Infância</a>
+                            <a href="https://www.brilhodainfancia.com.br/" target="_blank"><div className = "inline"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+        </svg></div> Brilho da Infância</a>
                         </h2>
                         <p className="m-4 text-lg p-4 leading-relaxed text-center ">
                             Ecommerce desenvolvido pela Phog Tech onde configuramos desde o DNS e SEO até a loja propriamente dita.
@@ -21,7 +22,9 @@ export default function Services() {
                     <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
 
                         <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
-                            <a href="https://www.koisinhasdakisy.com.br/" target="_blank"><ClickIcon></ClickIcon> Koisinhas da Kisy</a>
+                            <a href="https://www.koisinhasdakisy.com.br/" target="_blank"><div className = "inline"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+        </svg></div> Koisinhas da Kisy</a>
                         </h2>
                         <p className="m-4 text-lg p-4 leading-relaxed text-center ">
                             Ecommerce desenvolvido pela Phog Tech onde configuramos a loja e resolução de bugs, além de SEO.
@@ -30,7 +33,9 @@ export default function Services() {
 
                     <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
                         <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
-                            <a href="https://github.com/Phog-Tech/musa-da-mente" target="_blank"><ClickIcon></ClickIcon> Musa da Mente</a>
+                            <a href="https://github.com/Phog-Tech/musa-da-mente" target="_blank"><div className = "inline"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+        </svg></div> Musa da Mente</a>
                         </h2>
                         <p className="m-4 text-lg p-4 leading-relaxed text-center ">
                             Teste de personalidade com front-end em JavaScript e back-end em PHP
