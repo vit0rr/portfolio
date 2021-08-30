3fdfd5a0e730bbe0b19e2edf77b111fec286478b
Vitor <vi.souza.almeida@protonmail.com>
2021-08-30 15:06:14 -0300

    feat: adição de novo serviço

--- components/Services.js
+++ components/Services.js
@@ -6,7 +6,7 @@ export default function Services() {
                 Serviços
             </h2>
             <section className="container mx-auto py-10 md:py-20 antialiased align-middle inline-block">
-                <section className="grid lg:grid-cols-3 2xl:grid-cols-3 grid-cols-1 gap-8">
+                <section className="grid lg:grid-cols-4 2xl:grid-cols-4 grid-cols-1 gap-8">
 
                     <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
                         <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
@@ -39,6 +39,16 @@ export default function Services() {
                         </h2>
                         <p className="m-4 text-lg p-4 leading-relaxed text-center text-gray-300">
                             Teste de personalidade com front-end em JavaScript e back-end em PHP
+                        </p>
+                    </article>
+                    <article className="mx-auto max-w-sm pb-8 bg-black bg-opacity-50 bg-center  transform duration-500 hover:-translate-y-1 shadow-2xl rounded-xl">
+                        <h2 className="text-purple-600 hover:text-purple-700 text-center text-2xl mt-8 font-bold min-h-18 px-12">
+                            <a href="https://github.com/Phog-Tech/agro-bom-comercio" target="_blank"><div className = "inline"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+        </svg></div> Agro Bom Comércio</a>
+                        </h2>
+                        <p className="m-4 text-lg p-4 leading-relaxed text-center text-gray-300">
+                            Loja com fornecedores e clientes contruída do zero.
                         </p>
                     </article>
                 </section>
