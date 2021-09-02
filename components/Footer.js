dc708bd65e2bfa108736b42a8508a36204a35f5b
Vitor <vi.souza.almeida@protonmail.com>
2021-09-02 14:41:15 -0300

    feat: atributo blank no footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
           <span className="ml-3 text-xl">Vitor S. Almeida</span>
         </a>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a href="https://github.com/vit0rr/" className="mainFooter">
+          <a href="https://github.com/vit0rr/" className="mainFooter" target="_blank">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -21,7 +21,7 @@ export default function Footer() {
               <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
             </svg>
           </a>
-          <a className="ml-3 mainFooter" href="https://www.linkedin.com/in/vitorsalmeida/">
+          <a className="ml-3 mainFooter" href="https://www.linkedin.com/in/vitorsalmeida/" target="_blank">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
