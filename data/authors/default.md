89f49d5d3da576c1c9f42643534e32bae3b40c78
Vitor <vi.souza.almeida@protonmail.com>
2021-09-12 21:13:30 -0300

    feat: user blog

---
+++ data/authors/default.md
@@ -0,0 +1,16 @@
+---
+name: Vitor S. Almeida
+avatar: /static/images/naruto.jpg
+occupation: Desenvolvedor Web Front-End
+company:
+email: vi.souza.almeida@protonmail.com
+twitter: https://twitter.com/vit0rrk
+linkedin: https://www.linkedin.com/in/vitorsalmeida/
+github: https://github.com/vit0rr
+---
+
+Desenvolvedor Front-End. Em 2020 foi a primeira vez que participei do Hacktoberfest e consegui os prêmios. Estudo seriamente desenvolvimento há uns 2 anos.
+
+Gosto de open-source e participo de um projeto Linux brasileiro chamado Pindorama.
+
+Academicamente pretendo cursar Sistema da Informação e/ou Ciência da Computação. Futuramente quem sabe Filosofia.
---
+++ data/projectsData.ts
@@ -0,0 +1,28 @@
+const projectsData = [
+  {
+    title: 'Agro bom Comércio',
+    description: `Loja com fornecedores e clientes contruída do zero. Tive que identificar e auxiliar na correção de vulnerabilidades, colocando em prática um pouco do que sei sobre segurança da informação.`,
+    imgSrc: '/static/images/abc.png',
+    href: 'https://github.com/Phog-Tech/agro-bom-comercio',
+  },
+  {
+    title: 'Musa da Mente',
+    description: `Teste de personalidade com front-end em JavaScript e back-end em PHP`,
+    imgSrc: '/static/images/musadamente.png',
+    href: 'https://github.com/Phog-Tech/agro-bom-comercio',
+  },
+  {
+    title: 'Brilho da Infância',
+    description: `Ecommerce desenvolvido pela Phog Tech onde configuramos desde o DNS e SEO até a loja propriamente dita.`,
+    imgSrc: '/static/images/brilhoDaInfancia.png',
+    href: 'https://www.brilhodainfancia.com.br/',
+  },
+  {
+    title: 'Koisinhasa da Kisy',
+    description: `Ecommerce desenvolvido pela Phog Tech onde configuramos a loja e resolução de bugs, além de SEO.`,
+    imgSrc: '/static/images/koisinhasdakisy.png',
+    href: 'https://www.koisinhasdakisy.com.br/',
+  },
+]
+
+export default projectsData
