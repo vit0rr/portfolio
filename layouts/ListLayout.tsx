b11e364240ce57631818148708b32e426b8e59f0
Vitor <vi.souza.almeida@protonmail.com>
2021-09-17 17:06:02 -0300

    style: hover em Blog

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -26,7 +26,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
     <>
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 dark:hover:text-gray-400 hover:text-gray-500 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
           </h1>
           <div className="relative max-w-lg">
@@ -69,7 +69,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                   <div className="space-y-3 xl:col-span-3">
                     <div>
                       <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100 dark:hover:text-gray-400 hover:text-gray-200">
                           {title}
                         </Link>
                       </h3>
