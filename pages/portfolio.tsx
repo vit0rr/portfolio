0f28c2a981d3be984b0250b410cc0fcb7cdc95d5
Vitor <vi.souza.almeida@protonmail.com>
2021-11-13 23:47:47 -0300

    feat: remoção de freelas

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,5 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
-import projectsData from '@/data/projectsData'
+/* import projectsData from '@/data/projectsData' */
 import portfolioData from '@/data/portfolioData'
 import Card from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
@@ -8,7 +8,7 @@ export default function Projects() {
   return (
     <>
       <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      {/* <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Serviços
@@ -30,7 +30,7 @@ export default function Projects() {
             ))}
           </div>
         </div>
-      </div>
+      </div> */}
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
