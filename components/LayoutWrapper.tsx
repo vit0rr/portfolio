a984b189cdebf29d5f667331a05e6caf1c0d2d9b
Vitor S <vi.souza.almeida@protonmail.com>
2021-12-24 11:49:48 -0300

    issue: Vitor S. button hide in small device

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -24,7 +24,7 @@ const LayoutWrapper = ({ children }: Props) => {
                   <Logo />
                 </div> */}
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  <div className="h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
