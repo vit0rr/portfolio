e325b2664ab522a52f7dc39ba4f3b9c48769e591
Vitor S <vi.souza.almeida@protonmail.com>
2021-12-26 00:02:22 -0300

    feat: pinned link in navbar

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
+  { href: '/pinned', title: 'Pinned' },
   { href: '/tags', title: 'Tags' },
   // { href: '/portfolio', title: 'Portfolio' },
   { href: '/about', title: 'About' },
