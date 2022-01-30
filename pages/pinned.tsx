d1eb3032845ac8bc1cc4841601694ec041bd6503
Vitor <vi.souza.almeida@protonmail.com>
2022-01-30 15:58:17 -0300

    refactor: Pinned in order of data

--- pages/pinned.tsx
+++ pages/pinned.tsx
@@ -29,7 +29,87 @@ export default function Home() {
                 <dl>
                   <dt className="sr-only">Published on</dt>
                   <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime="2021-12-24T03:00:00.000Z">December 24, 2021</time>
+                    <time dateTime="2022-01-18T00:00:00.000Z">Jan 17, 2022</time>
+                  </dd>
+                </dl>
+                <div className="space-y-5 xl:col-span-3">
+                  <div className="space-y-6">
+                    <div>
+                      <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                        <Link
+                          href={`/blog/learning-curve`}
+                          className="text-gray-900 dark:text-gray-100 dark:hover:text-gray-400 hover:text-gray-500"
+                        >
+                          Stop thinking about learning curve
+                        </Link>
+                      </h2>
+                    </div>
+                    <div className="prose text-gray-500 max-w-none dark:text-gray-400">
+                      Stop thinking about learning curve
+                    </div>
+                  </div>
+                  <div className="text-base font-medium leading-6">
+                    <Link
+                      href={`/blog/learning-curve`}
+                      className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
+                      aria-label={`Read "Why experience time requirement is bullshit"`}
+                    >
+                      See more &rarr;
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </li>
+        </ul>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          <li className="py-12">
+            <article>
+              <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
+                <dl>
+                  <dt className="sr-only">Published on</dt>
+                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                    <time dateTime="2021-12-31T03:00:00.000Z">Dec 31, 2021</time>
+                  </dd>
+                </dl>
+                <div className="space-y-5 xl:col-span-3">
+                  <div className="space-y-6">
+                    <div>
+                      <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                        <Link
+                          href={`/blog/my-first-english-interview`}
+                          className="text-gray-900 dark:text-gray-100 dark:hover:text-gray-400 hover:text-gray-500"
+                        >
+                          Lessons learned in my first English interview
+                        </Link>
+                      </h2>
+                    </div>
+                    <div className="prose text-gray-500 max-w-none dark:text-gray-400">
+                      How was my first English interview about blockchain development work
+                    </div>
+                  </div>
+                  <div className="text-base font-medium leading-6">
+                    <Link
+                      href={`/blog/my-first-english-interview`}
+                      className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
+                      aria-label={`Read "Why experience time requirement is bullshit"`}
+                    >
+                      See more &rarr;
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </li>
+        </ul>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          <li className="py-12">
+            <article>
+              <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
+                <dl>
+                  <dt className="sr-only">Published on</dt>
+                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                    <time dateTime="2021-12-24T03:00:00.000Z">Dec 24, 2021</time>
                   </dd>
                 </dl>
                 <div className="space-y-5 xl:col-span-3">
@@ -70,7 +150,7 @@ export default function Home() {
                 <dl>
                   <dt className="sr-only">Published on</dt>
                   <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime="2021-12-01T00:00:00.000Z">November 30, 2021</time>
+                    <time dateTime="2021-12-01T00:00:00.000Z">Nov 30, 2021</time>
                   </dd>
                 </dl>
                 <div className="space-y-5 xl:col-span-3">
@@ -111,7 +191,7 @@ export default function Home() {
                 <dl>
                   <dt className="sr-only">Published on</dt>
                   <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime="2021-11-23T00:00:00.000Z">November 22, 2021</time>
+                    <time dateTime="2021-11-23T00:00:00.000Z">Nov 22, 2021</time>
                   </dd>
                 </dl>
                 <div className="space-y-5 xl:col-span-3">
@@ -134,88 +214,6 @@ export default function Home() {
                   <div className="text-base font-medium leading-6">
                     <Link
                       href={`/blog/job-with-1-article`}
-                      className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
-                      aria-label={`Read "Why experience time requirement is bullshit"`}
-                    >
-                      See more &rarr;
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </article>
-          </li>
-        </ul>
-
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          <li className="py-12">
-            <article>
-              <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
-                <dl>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime="2021-12-31T03:00:00.000Z">December 31, 2021</time>
-                  </dd>
-                </dl>
-                <div className="space-y-5 xl:col-span-3">
-                  <div className="space-y-6">
-                    <div>
-                      <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link
-                          href={`/blog/my-first-english-interview`}
-                          className="text-gray-900 dark:text-gray-100 dark:hover:text-gray-400 hover:text-gray-500"
-                        >
-                          Lessons learned in my first English interview
-                        </Link>
-                      </h2>
-                    </div>
-                    <div className="prose text-gray-500 max-w-none dark:text-gray-400">
-                      How was my first English interview about blockchain development work
-                    </div>
-                  </div>
-                  <div className="text-base font-medium leading-6">
-                    <Link
-                      href={`/blog/my-first-english-interview`}
-                      className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
-                      aria-label={`Read "Why experience time requirement is bullshit"`}
-                    >
-                      See more &rarr;
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </article>
-          </li>
-        </ul>
-
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          <li className="py-12">
-            <article>
-              <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
-                <dl>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime="2022-01-18T00:00:00.000Z">January 17, 2022</time>
-                  </dd>
-                </dl>
-                <div className="space-y-5 xl:col-span-3">
-                  <div className="space-y-6">
-                    <div>
-                      <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link
-                          href={`/blog/learning-curve`}
-                          className="text-gray-900 dark:text-gray-100 dark:hover:text-gray-400 hover:text-gray-500"
-                        >
-                          Stop thinking about learning curve
-                        </Link>
-                      </h2>
-                    </div>
-                    <div className="prose text-gray-500 max-w-none dark:text-gray-400">
-                      Stop thinking about learning curve
-                    </div>
-                  </div>
-                  <div className="text-base font-medium leading-6">
-                    <Link
-                      href={`/blog/learning-curve`}
                       className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
                       aria-label={`Read "Why experience time requirement is bullshit"`}
                     >
