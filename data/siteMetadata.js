f33a2f71cd99455d1137f8209e6dffdd9eee9d63
Vitor <vi.souza.almeida@protonmail.com>
2022-01-31 00:12:24 -0300

    refactor: Title to Vitor's Blog

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,5 +1,5 @@
 const siteMetadata = {
-  title: 'Vitor S.',
+  title: "Vitor's Blog",
   author: 'Vitor S. Almeida',
   headerTitle: "Vitor's Blog",
   description: 'My lasted posts',
@@ -15,50 +15,32 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com/in/vitorsalmeida/',
   locale: 'en-US',
   analytics: {
-    // supports plausible, simpleAnalytics or googleAnalytics
-    plausibleDataDomain: 'vitorsalmeida.com', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: true, // true or false
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    plausibleDataDomain: 'vitorsalmeida.com',
+    simpleAnalytics: true,
+    googleAnalyticsId: '',
   },
   comment: {
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: 'giscus',
     giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
       repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+      mapping: 'pathname',
+      reactions: '1',
       metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
       theme: 'light',
-      // theme when dark mode
       darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
     },
     utterancesConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://utteranc.es/
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      issueTerm: '', // supported options: pathname, url, title
-      label: '', // label (optional): Comment 💬
-      // theme example: github-light, github-dark, preferred-color-scheme
-      // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
+      issueTerm: '',
+      label: '',
       theme: '',
-      // theme when dark mode
       darkTheme: '',
     },
     disqus: {
-      // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
       shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
     },
   },
