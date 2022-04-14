33ff6d49a1e3fa50cae2f259c53a1652eae1f275
Vitor <vi.souza.almeida@protonmail.com>
2022-04-14 20:34:46 -0300

    refactor: Bold titles

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -75,7 +75,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                   </dl>
                   <div className="space-y-3 xl:col-span-3">
                     <div>
-                      <h3 className="text-2xl leading-8 tracking-tight">
+                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
                         <Link
                           href={`/blog/${slug}`}
                           className="text-gray-900 dark:text-gray-100 dark:hover:text-gray-400 hover:text-gray-500"
