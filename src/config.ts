0713f4d5a4a10a611e65d92088014a83e47d3928
vitor <vi.souza.almeida@protonmail.com>
2024-11-05 20:37:37 -0300

    feat(blog): add blog to navlink (#209)

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const SITE = {
   title: "vitorsalmeida",
   website: 'https://vitorsalmeida.com',
-  description: "Thoughts. Learning about math, computer science, programming languages and software development.",
-  author: 'Vitor S. Almeida',
+  description: "thoughts. learning about math, computer science, programming languages and software development.",
+  author: 'vitor s. almeida',
 }
 
 export const GITHUB_REPO_URL = 'https://github.com/vit0rr/';
