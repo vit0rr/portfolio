de8a67eb92a42b1c9210f2c44643b9bcf0cdf349
vitor <vi.souza.almeida@protonmail.com>
2024-11-05 20:47:04 -0300

    feat(blog): make rss work

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -7,19 +7,54 @@ import { SITE } from "../config";
 
 const parser = new MarkdownIt();
 
-export async function get() {
+export async function GET() {
   const posts = await getCollection("blog", ({ data }) => !data.draft);
+
+  const items = posts
+    .map(({ data, slug, body }) => ({
+      link: slug,
+      title: data.title,
+      description: data.description,
+      pubDate: data.pubDate,
+      content: sanitizeHtml(
+        parser.render(body),
+        {
+          allowedTags: [
+            'img',
+            'a',
+            'p',
+            'strong',
+            'b',
+            'i',
+            'em',
+            'hr',
+            'h1',
+            'h2',
+            'h3',
+            'h4',
+            'h5',
+            'h6',
+            'ul',
+            'li',
+            'code',
+            'pre',
+            'table',
+            'tr',
+            'td',
+            'thead',
+            'tbody',
+            'th',
+            'blockquote',
+          ]
+        }
+      ),
+    }));
 
   return rss({
     title: SITE.title,
     description: SITE.description,
     site: SITE.website,
-    items: posts.map(({ data, slug, body }) => ({
-      link: slug,
-      title: data.title,
-      description: data.description,
-      pubDate: data.pubDate,
-      content: sanitizeHtml(parser.render(body))
-    })),
+    items,
+    // stylesheet: '/rss.xsl',
   });
 }
