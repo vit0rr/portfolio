c81744883f00ab272d3fbdf4589609a164360e3b
vitor <vi.souza.almeida@protonmail.com>
2024-11-06 10:14:34 -0300

    refactor(blog): fix redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,12 +10,14 @@ import vercel from "@astrojs/vercel/static";
 const BLOG_DIR = './src/content/blog';
 
 const getBlogRoutesRedirect = async () => {
-  const blogRoutes = (await fs.readdir(BLOG_DIR)).map((slug) => [
+  const blogRoutes = await fs.readdir(BLOG_DIR)
+
+  const slugs = blogRoutes.map((slug) => [
     `/${slug}`,
     `/blog/${slug}`,
   ]);
 
-  return Object.fromEntries(blogRoutes);
+  return Object.fromEntries(slugs);
 }
 
 // https://astro.build/config
