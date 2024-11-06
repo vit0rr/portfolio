5127cb9021c0d6aa3babd36dbab4bb59ef3dfe1f
vitor <vi.souza.almeida@protonmail.com>
2024-11-06 10:09:08 -0300

    refactor(blog): typo getBlogRoutesRedirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ const BLOG_DIR = './src/content/blog';
 
 const getBlogRoutesRedirect = async () => {
   const blogRoutes = (await fs.readdir(BLOG_DIR)).map((slug) => [
-    `${slug}`,
+    `/${slug}`,
     `/blog/${slug}`,
   ]);
 
